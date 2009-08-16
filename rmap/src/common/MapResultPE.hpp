ed07d4e2ac6d04ea27428954fb8d8c407b56a3b3
andrewdavidsmith <andrew@smithlabresearch.org>
2009-08-16 02:51:07 +0000

    Added the MapResult objects that hold the individual end mapping candidates when doing paired end in cheat mode.

--- rmap/src/common/MapResultPE.hpp
+++ rmap/src/common/MapResultPE.hpp
@@ -34,7 +34,7 @@ struct MapResultPE {
   void set(size_t ste, size_t st2, size_t chr, bool str);
   unsigned site   : 32;
   unsigned site2  : 32;
-  unsigned chrom  : 15;
+  unsigned chrom  : 31;
   unsigned strand : 1;
   bool operator<(const MapResultPE& rhs) const {
     return (chrom < rhs.chrom ||
@@ -72,7 +72,7 @@ struct MultiMapResultPE {
   }
   void add(size_t scr, size_t chr, size_t ste, size_t st2, bool str) {
     if (scr < score) {
-      mr.clear();
+      mr.resize(0);
       score = scr;
     }
     // The "<=" below is not because we want to keep one more than
@@ -98,5 +98,67 @@ struct MultiMapResultPE {
 
 size_t MultiMapResultPE::max_count;
 size_t MultiMapResultPE::twice_max_count;
+
+struct MapResult {
+  MapResult(size_t ste = std::numeric_limits<size_t>::max(),
+	    size_t chr = 0,
+	    bool str = true) : site(ste), chrom(chr), strand(str) {}
+  void set(size_t ste, size_t chr, bool str);
+  unsigned site   : 32;
+  unsigned chrom  : 31;
+  unsigned strand : 1;
+  bool operator<(const MapResult& rhs) const {
+    return (chrom < rhs.chrom || (chrom == rhs.chrom && site < rhs.site));
+  }
+  bool operator==(const MapResult& rhs) const {
+    return site == rhs.site && chrom == rhs.chrom;
+  }
+  std::string tostring() const {
+    return rmap::toa(site) + "\t" + rmap::toa(chrom) + "\t" \
+      + rmap::toa(strand);
+  }
+};
+
+inline void 
+MapResult::set(size_t ste, size_t chr, bool str) {
+  site = ste;
+  chrom = chr;
+  strand = str;
+}
+
+class MultiMapResult {
+public:
+  MultiMapResult(size_t scr) : score(scr) {}
+  bool empty() const {return mr.empty();}
+  void sort() {std::sort(mr.begin(), mr.end());}
+  void clear() {std::vector<MapResult>().swap(mr);}
+  void swap(MultiMapResult &rhs) {
+    mr.swap(rhs.mr);
+    std::swap(score, rhs.score);
+  }
+  void add(size_t scr, size_t chr, size_t ste, bool str) {
+    if (mr.size() <= twice_max_count)
+      mr.push_back(MapResult(ste, chr, str));
+    else if (scr < score)
+      mr.back() = MapResult(ste, chr, str);
+    score = std::min(scr, score);
+  }
+  size_t score;
+  std::vector<MapResult> mr;
+  bool ambiguous() const {return mr.size() > max_count;}
+  void collapse() {
+    std::sort(mr.begin(), mr.end());
+    mr.erase(std::unique(mr.begin(), mr.end()), mr.end());
+  }
+  static size_t max_count;
+  static size_t twice_max_count;
+  static void init(const size_t mc) {
+    max_count = mc;
+    twice_max_count = 2*mc;
+  }
+};
+
+size_t MultiMapResult::max_count;
+size_t MultiMapResult::twice_max_count;
 
 #endif
